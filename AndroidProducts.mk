3199ca5ee8128badf484538284ad2d286898915c
samuelKuseka <57397205+samuelKuseka@users.noreply.github.com>
2022-04-16 08:12:37 +0200

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_TECNO-CD8j.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_TECNO-CD8j-user \
+    omni_TECNO-CD8j-userdebug \
+    omni_TECNO-CD8j-eng
---
+++ omni_TECNO-CD8j.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from TECNO-CD8j device
+$(call inherit-product, device/tecno/TECNO-CD8j/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := TECNO-CD8j
+PRODUCT_NAME := omni_TECNO-CD8j
+PRODUCT_BRAND := TECNO
+PRODUCT_MODEL := TECNO CD8j
+PRODUCT_MANUFACTURER := tecno
