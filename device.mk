eed713353d8c60bfc07c319fada5a39019acf123
samuelKuseka <57397205+samuelKuseka@users.noreply.github.com>
2022-04-20 12:51:09 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -7,6 +7,11 @@
 
 LOCAL_PATH := device/tecno/TECNO_CD8j
 
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
